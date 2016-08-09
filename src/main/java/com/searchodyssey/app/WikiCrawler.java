8c915218bdb4a89bd532c7f0cef5e5cf078eadc5
Hayley Call <hayleycall@utexas.edu>
2016-08-09 10:51:34 -0700

    decreased amount of links

--- src/main/java/com/searchodyssey/app/WikiCrawler.java
+++ src/main/java/com/searchodyssey/app/WikiCrawler.java
@@ -88,7 +88,7 @@ public class WikiCrawler {
                                         for(Element f : links) {
                                                 url=((Element) node).attr("href");
                                                 if(url.startsWith("/wiki/")) {
-                                                	if (queueSize() <= 100) {
+                                                	if (queueSize() <= 2) {
                                                         queue.add("https://en.wikipedia.org"+url);
                                                      } else {
                                                      	return;
